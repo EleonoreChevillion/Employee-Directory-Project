3ccc4bde0caf3c4168b3814491d80c3b7337526e
Eleonore Chevillion <el.chevillion@gmail.com>
2019-02-23 11:54:16 -0500

    firebase start

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,3 +8,56 @@ var config = {
   messagingSenderId: "182569030754"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var fname = "";
+var lname = "";
+var email = "";
+var title = "";
+var phone = "";
+var hiredate = "";
+var homeaddress = "";
+var salary;
+
+$("#submit").on("click", function(event) {
+  // Don't refresh the page!
+  event.preventDefault();
+
+  // YOUR TASK!!!
+
+  name = $("#name-input")
+    .val()
+    .trim();
+  title = $("#title-input")
+    .val()
+    .trim();
+  phone = $("#phone-number-input")
+    .val()
+    .trim();
+  email = $("#email-address-input")
+    .val()
+    .trim();
+
+  hiredate = $("#hire-date-input")
+    .val()
+    .trim();
+  salary = $("#salary-input")
+    .val()
+    .trim();
+
+  database.ref().push({
+    fname: fname,
+    lname: lname,
+    email: email,
+    homeaddress: homeaddress,
+    hiredate: hiredate,
+    phone: phone,
+    salary: salary,
+    title: title
+  });
+  // Change what is saved in firebase
+});
+database.ref().on("child_append", function(snapshot) {
+  // If Firebase has a highPrice and highBidder stored, update our client-side variables
+});
